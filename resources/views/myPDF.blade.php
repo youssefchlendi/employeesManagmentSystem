47ea3229f4303b6da5ad6fb927a2adabcefe85ad
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-05 00:54:16 +0100

    update title

--- resources/views/myPDF.blade.php
+++ resources/views/myPDF.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>Aloha!</title>
+<title>Fiche de paie</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <style type="text/css">
     /* * {
