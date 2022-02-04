9170b6f349664302804480294c1d0edcf355d473
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-04 17:06:17 +0100

    add oneRebrique tab

--- resources/js/components/fiche/show.vue
+++ resources/js/components/fiche/show.vue
@@ -1,26 +1,29 @@
 <template>
 <div>
-    <div class="card card-body my-2" v-if="fiches.length==0">
-          <h3>il y'a aucun fiche</h3>
-      </div>
  <b-card v-for="fiche in fiches" :key="fiche.id">
-    <b-card-header header-tag="nav">
-      <b-nav card-header tabs>
-        <b-nav-item active>Fiches</b-nav-item>
-      </b-nav>
-    </b-card-header>
-
-
+    <b-tabs>
+        <b-tab title="Fiches">
         <b-card-body>
-        <b-container class="bv-example-row text-center">
-        <b-row  class="mb-2">
-        <b-row><b-col>date : {{ fiche.date }} employe :  {{fiche.employe_id }}</b-col></b-row>
-        <b-row><b-col>total : {{ fiche.total}}</b-col></b-row>
-        </b-row>
-      <b-button variant="danger" v-on:click="Delete(fiche.id)" >Delete</b-button>
-      <b-button variant="warning" v-on:click="Update(fiche)" data-bs-toggle="modal" data-bs-target="#ficheModal" >Update</b-button>
-    </b-container>
-    </b-card-body>
+            <h6 v-if="fiches.length==0">Aucun employe </h6>
+            <b-container class="bv-example-row text-center">
+            <b-row  class="mb-2">
+            <b-row><b-col>date : {{ fiche.date }} employe :  {{fiche.employe_id }}</b-col></b-row>
+            <b-row><b-col>total : {{ fiche.total}}</b-col></b-row>
+            </b-row>
+            <b-button variant="danger" v-on:click="Delete(fiche.id)" >Delete</b-button>
+            <b-button variant="warning" v-on:click="Update(fiche)" data-bs-toggle="modal" data-bs-target="#ficheModal" >Update</b-button>
+            </b-container>
+        </b-card-body>
+        </b-tab>
+        <b-tab title="Fiches">
+        <b-card-body>
+            <h6 v-if="fiche.rebriques.length==0">Aucunu rebrique affectée </h6>
+            <b-container class="bv-example-row text-center">
+                <oneRebrique :rebrique="rebrique" v-for="rebrique in fiche.rebriques" :key="rebrique.id" />
+            </b-container>
+        </b-card-body>
+        </b-tab>
+    </b-tabs>
   </b-card>
         <nav  class="row ">
           <ul class="pagination w-auto mx-auto">
@@ -38,7 +41,11 @@
 </template>
 
 <script>
+import oneRebrique from './oneRebrique.vue';
 export default {
+    components : {
+        oneRebrique
+    },
     props:{
         fiches : Array,
         pagination : Object,
