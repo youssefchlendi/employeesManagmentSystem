7cf42ae0d5cf5c2d941377c268c8e78caa2f5fc6
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-04 17:05:58 +0100

    component rubrique set/get montant

---
+++ resources/js/components/fiche/oneRebrique.vue
@@ -0,0 +1,61 @@
+<template>
+    <li class="container list-unstyled">
+    <div class="row shadow-sm text-left">
+    <div class="">
+        <p class="text-left"> {{rebrique.titre}} </p>
+        <b-input-group
+        class="mb-3"
+        prepend="Montant"
+        >
+        <b-form-input v-on:keyup.enter="updateMontant(rebrique.pivot.montant)" v-model="rebrique.pivot.montant" ></b-form-input>
+        <b-input-group-append>
+        <b-button  text="Modifier" variant="success" @click="updateMontant(rebrique.pivot.montant)">Modifier</b-button>
+        </b-input-group-append>
+        </b-input-group>
+    </div>
+    </div>
+    </li>
+</template>
+
+<script>
+export default {
+    props : {
+        rebrique : Object
+    },
+    data(){
+        return {
+
+        }
+    },
+    methods : {
+        containsObject(){
+
+        },
+        pushTo(){
+
+        },
+        updateMontant(){
+            fetch('api/fiche/'+this.rebrique.pivot.fiche_id+'/rebrique/'+this.rebrique.id, {
+                method: 'PUT',
+                body: JSON.stringify({'montant': this.rebrique.pivot.montant}),
+                headers: {
+                    "Content-Type": 'application/json'
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+                        this.fiche.id=data.data.id;
+                        this.fiche.rebriques.forEach(r=>this.attachRebrique(r.id));
+                        this.fetchFiches();
+                    }
+                )
+                .catch(err => console.log(err))
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
