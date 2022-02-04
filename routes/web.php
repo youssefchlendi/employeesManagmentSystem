612092acc8ec86fcd71141619d280bea0f0d86bb
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-05 00:24:14 +0100

    generate pdf route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,8 +11,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
+Route::get('/generatePdf/{id}/fiche/{fid}',[App\Http\Controllers\PdfController::class, 'generatePDF']);
 Route::get('/{any?}', [
     function () {
         return view('vue');
