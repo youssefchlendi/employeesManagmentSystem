ffea209d6ac2ec6428adab53e7f87096826a0972
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-05 23:54:55 +0100

    add full rebrique crud

--- resources/js/components/rebrique/Rebrique.vue
+++ resources/js/components/rebrique/Rebrique.vue
@@ -1,15 +1,165 @@
 <template>
-<div>
-    asd
-</div>
+    <div class="mt-4">
+        <b-overlay v-if="show" :show="show" class="d-inline-block" style="height:500px;width:100%"></b-overlay>
+        <div v-if="!show">
+                <div class="input-group mb-4" style="width: 50%; left:25%">
+                    <input
+                        type="text"
+                        class="form-control"
+                        id="rebname"
+                        placeholder="titre"
+                        v-model="Rebrique.titre"
+                    />
+                    <div class="input-group-append">
+                        <button
+                            type="button"
+                            @click="addRebrique"
+                            class="btn btn-success"
+                        >Sauvegarder</button>
+                    </div>
+                </div>
+        <b-alert
+            :show="alert.dismissCountDown"
+            dismissible
+            :variant="alert.variant"
+            @dismissed="alert.dismissCountDown = 0"
+        >
+            <p>{{ alert.msg }}</p>
+        </b-alert>
+        <b-card>
+            <li v-for="(rebrique) in Rebriques" :key="rebrique.id" class="container list-unstyled">
+                <div class="row shadow-sm">
+                    <div class="col-10">
+                        <div>
+                            <label @dblclick="rebrique.edit = true">{{ rebrique.titre }}</label>
+                        </div>
+                    </div>
+                    <div class="col-2">
+                        <button
+                            type="button"
+                            class="float-end btn"
+                            @click="deleteRebrique(rebrique.id)"
+                        >
+                            <b-icon icon="trash" scale="1" variant="danger"></b-icon>
+                        </button>
+                        <button
+                            type="button"
+                            class="float-end btn"
+                            @click="updateRebrique(rebrique)"
+                        >
+                            <b-icon icon="pen" scale="1" variant="warning"></b-icon>
+                        </button>
+                    </div>
+                </div>
+            </li>
+        </b-card>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
 
+    data() {
+        return {
+            visible: true,
+            Rebriques: [],
+            Rebrique: { titre: "", id: 'false' },
+            check: [],
+            edit: false,
+            show: true,
+            alert: {
+                dismissCountDown: 0,
+                variant: "",
+                msg: "",
+            }
+        }
+    },
+    created() {
+        this.fetchRebriques();
+    },
+    methods: {
+        fetchRebriques(page_url = "/api/rebrique") {
+            let vm = this;
+            fetch(page_url, {
+                method: 'GET',
+            })
+                .then(res => res.json())
+                .then(res => {
+                    this.Rebriques = res;
+                    this.show = false;
+                })
+                .catch(err => console.log(err))
+        },
+        deleteRebrique(id) {
+            if (confirm('Delete Rebrique ' + id)) {
+                fetch('api/rebrique/' + id, { method: 'delete' })
+                    .then(res => {
+                        this.fetchRebriques();
+                        this.alert.variant = "danger";
+                        this.alert.msg = "Rebrique suprimée avec succès"
+                        this.alert.dismissCountDown = 5;
+
+                    })
+                    .then(data => {
+                    })
+                    .catch(err => console.log(err));
+            }
+        },
+        resetModal1() {
+            this.rebrique = {};
+        },
+        addRebrique() {
+            if (!this.edit) {
+                fetch('api/rebrique/', {
+                    method: 'post',
+                    body: JSON.stringify(this.Rebrique),
+                    headers: {
+                        "Content-Type": 'application/json'
+                    }
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        this.Rebrique.titre = "";
+                        this.alert.variant = "success";
+                        this.alert.msg = "rebriqe ajoutée avec succès"
+                        this.alert.dismissCountDown = 5;
+                        this.fetchRebriques();
+                    }
+                    )
+                    .catch(err => console.log(err))
+            } else {
+                fetch('api/rebrique/' + this.Rebrique.id, {
+                    method: 'put',
+                    body: JSON.stringify(this.Rebrique),
+                    headers: {
+                        "Content-Type": 'application/json'
+                    }
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        this.fetchRebriques();
+                        this.alert.variant = "warning";
+                        this.alert.msg = "rebriqe modifiée avec succès"
+                        this.alert.dismissCountDown = 5;
+                        this.Rebrique = { titre: "", id: 'false' };
+                        this.edit = false;
+                    }
+                    )
+                    .catch(err => console.log(err))
+            }
+        },
+        updateRebrique(Rebrique) {
+            this.edit = true;
+            this.Rebrique = Rebrique;
+        },
+
+    },
+    computed: {
+
+    }
 }
 </script>
 
 <style>
-
 </style>
