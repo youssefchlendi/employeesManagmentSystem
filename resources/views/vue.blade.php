234184d62e432be9ffc11276c7e2943a7b4d4783
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-09 13:19:39 +0100

    import jquery

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -20,7 +20,8 @@
         <div class="container">
             <router-view class="contain" id="contain" ></router-view>
         </div>
-</div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </body>
 </html>
 
