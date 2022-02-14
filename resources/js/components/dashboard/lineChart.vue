fe00c9486ea7e13e21e4d6f4e73067ce46feaaee
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-14 14:31:30 +0100

    Create linechart component

---
+++ resources/js/components/dashboard/lineChart.vue
@@ -0,0 +1,32 @@
+<script>
+import { Line } from 'vue-chartjs';
+
+export default {
+  extends: Line,
+    props: ['labels','data'],
+  mounted() {
+    this.renderChart(
+      {
+        labels: this.labels,
+        datasets: [
+          {
+            label: "fiches",
+            data: this.data,
+            backgroundColor: "white",
+            borderColor: "white",
+            pointBackgroundColor: "white"
+          }
+        ]
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+          display: true,
+          text: "My Data"
+        }
+      }
+    );
+  }
+};
+</script>
