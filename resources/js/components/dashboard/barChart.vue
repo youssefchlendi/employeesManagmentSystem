c3c9d5156d2703939217292a779acfc267980445
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-14 14:31:44 +0100

    create barChart component

---
+++ resources/js/components/dashboard/barChart.vue
@@ -0,0 +1,32 @@
+<script>
+import { Bar } from 'vue-chartjs';
+
+export default {
+  extends: Bar,
+    props: ['labels','data'],
+  mounted() {
+    this.renderChart(
+      {
+        labels: this.labels,
+        datasets: [
+          {
+            label: "Employé/entreprise",
+            data: this.data,
+            backgroundColor: "#FDE7E7",
+            borderColor: "white",
+            pointBackgroundColor: "white"
+          }
+        ]
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+          display: true,
+          text: "My Data"
+        }
+      }
+    );
+  }
+};
+</script>
