20c0b91acbf89d9ea9c9b7f0366436dffa23e14d
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-14 19:27:28 +0100

    create topnav component

---
+++ resources/js/components/topnav.vue
@@ -0,0 +1,74 @@
+<template>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <!-- Container wrapper -->
+        <div class="container-fluid">
+            <!-- Toggle button -->
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-mdb-toggle="collapse"
+                data-mdb-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <i class="fas fa-bars"></i>
+            </button>
+            <a class="navbar-brand mt-2 mt-lg-0" href="#">
+                <img src="images/large.png" width="50" height="50" alt="MDB Logo" loading="lazy" />
+            </a>
+            <search v-if="show=='true'" :search="search" @fetch="fetch" />
+            <!-- Collapsible wrapper -->
+            <!-- Collapsible wrapper -->
+
+            <!-- Right elements -->
+            <div class="d-flex align-items-center">
+                <b-dropdown
+                    id="dropdown-right"
+                    right
+                    text="Right align"
+                    variant="primary"
+                    class="m-2"
+                >
+                    <template #button-content>
+                        <i class="fas fa-user"></i>
+                    </template>
+                    <b-dropdown-item href="#">Logout</b-dropdown-item>
+                    <b-dropdown-item href="#">Logout</b-dropdown-item>
+                </b-dropdown>
+            </div>
+            <!-- Right elements -->
+        </div>
+        <!-- Container wrapper -->
+    </nav>
+    <!-- Navbar -->
+</template>
+
+<script>
+import search from './search.vue';
+export default {
+    props:{
+        show:{
+            type:String,
+            default:false,
+        }
+    },
+    components: {
+        search
+    },
+    data(){
+        return{
+            search:'',
+        }
+    },
+    methods:{
+        fetch(search) {
+            this.$emit('fetch', search)
+        }
+    }
+}
+</script>
+
+<style>
+</style>
