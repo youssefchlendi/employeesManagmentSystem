91c8ef2ef8a03b8068144ddf7be3c97ef8cf2fad
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-02-14 14:31:55 +0100

    create areaChart component

---
+++ resources/js/components/dashboard/areaChart.vue
@@ -0,0 +1,35 @@
+<script>
+import { Bar } from "vue-chartjs";
+
+export default {
+  extends: Bar,
+      props: ['labels','data'],
+
+  data() {
+    return {
+    };
+  },
+  mounted() {
+
+    this.renderChart(
+      {
+        labels: this.labels,
+
+        datasets: [
+          {
+            label: "Salaire moyenne/entreprise",
+            data: this.data,
+            backgroundColor: "#FDE7E7",
+            borderColor: "white",
+            pointBackgroundColor: "white"
+          }
+        ]
+      },
+      { responsive: true, maintainAspectRatio: false ,title: {
+          display: true,
+          text: "My Data"
+        }}
+    );
+  }
+};
+</script>
